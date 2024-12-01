8b5812c41dc30b1b640fd15ba7823f8e7143246b
Erik Oudsen <eoudsen@users.noreply.github.com>
2024-12-01 07:30:40 +0100

    Day 1 implementation

--- java/src/days/day1/Day1.java
+++ java/src/days/day1/Day1.java
@@ -2,16 +2,52 @@ package days.day1;
 
 import util.Util;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
 
 public class Day1 {
 
     public static Integer part1(final List<String> input) {
-        return 0;
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        input.forEach(line -> {
+            left.add(Integer.parseInt(line.split(" {3}")[0]));
+            right.add(Integer.parseInt(line.split(" {3}")[1]));
+        });
+        left.sort(Comparator.naturalOrder());
+        right.sort(Comparator.naturalOrder());
+
+        int diff = 0;
+        for (int i = 0; i < left.size(); i++) {
+            diff += Math.abs(left.get(i) - right.get(i));
+        }
+
+        return diff; // 2815556
     }
 
-    public static Integer part2(final List<String> input) {
-        return 0;
+    public static Long part2(final List<String> input) {
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        input.forEach(line -> {
+            left.add(Integer.parseInt(line.split(" {3}")[0]));
+            right.add(Integer.parseInt(line.split(" {3}")[1]));
+
+        });
+
+        Map<Integer, Long> collect = right.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        AtomicLong similar = new AtomicLong();
+        left.forEach(value -> {
+            if (collect.containsKey(value)) {
+                similar.addAndGet(value * collect.get(value));
+            }
+        });
+
+        return similar.get(); // 23927637
     }
 
     public static void main(String... args) {
